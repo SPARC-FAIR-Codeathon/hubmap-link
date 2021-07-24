416f9e2eac0ee4e44744e7a844e97e6186164d7b
Bruce W. Herr II <bherr@indiana.edu>
2021-07-24 10:39:35 -0400

    Further fleshing out of the sparc jsonld data

--- api/src/routes/sparc-datasets.ts
+++ api/src/routes/sparc-datasets.ts
@@ -5,8 +5,9 @@ import { apiKey } from '../functions/api-key';
 
 export function setupSparcDatasetsRoute(app: Express): void {
   app.get('/sparc-datasets', async (req: Request, res: Response) => {  
+    const debug = req.query.format !== 'jsonld';
     try {
-      const response = await fetchSparcDatasets(apiKey);
+      const response = await fetchSparcDatasets(apiKey, debug);
       res.status(200).send(response);
     } catch (err) {
       console.log(err);
