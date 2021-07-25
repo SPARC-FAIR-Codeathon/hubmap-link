139787c61be86896be3eccc06dede1993c13dff5
Bruce W. Herr II <bherr@indiana.edu>
2021-07-25 10:14:13 -0400

    Minor cleanup

--- website/src/app/components/dataset-linkages/dataset-linkages-sidebar/dataset-linkages-sidebar.component.ts
+++ website/src/app/components/dataset-linkages/dataset-linkages-sidebar/dataset-linkages-sidebar.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { MetaDataService } from 'src/app/services/meta-data.service';
+import { Component } from '@angular/core';
+import { MetaDataService } from '../../../services/meta-data.service';
+
 
 @Component({
   selector: 'app-dataset-linkages-sidebar',
   templateUrl: './dataset-linkages-sidebar.component.html',
   styleUrls: ['./dataset-linkages-sidebar.component.css']
 })
-export class DatasetLinkagesSidebarComponent implements OnInit {
+export class DatasetLinkagesSidebarComponent {
 
   constructor(public metaDataService: MetaDataService) { }
-
-  
-
-  ngOnInit(): void {
-  }
-
 }
--- website/src/app/components/dataset-linkages/dataset-linkages-table-view/dataset-linkages-table-view.component.ts
+++ website/src/app/components/dataset-linkages/dataset-linkages-table-view/dataset-linkages-table-view.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { MetaDataService } from 'src/app/services/meta-data.service';
+import { Component } from '@angular/core';
+import { MetaDataService } from '../../../services/meta-data.service';
+
 
 @Component({
   selector: 'app-dataset-linkages-table-view',
   templateUrl: './dataset-linkages-table-view.component.html',
   styleUrls: ['./dataset-linkages-table-view.component.css']
 })
-export class DatasetLinkagesTableViewComponent implements OnInit {
+export class DatasetLinkagesTableViewComponent {
 
   constructor(public metaDataService:MetaDataService) { }
 
-  ngOnInit(): void {
-  }
-
-  getObjectKeys(obj:any):string[]{
+  getObjectKeys(obj: unknown): string[] {
     return Object.keys(obj);
   }
 }
